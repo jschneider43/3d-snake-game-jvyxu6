a7e862615f5aaecfd5beab034aa20848c8a6aff9
jschneider43 <jschneider1@lions.molloy.edu>
2021-01-31 15:12:25 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 3d-snake-game-jvyxu6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/3d-snake-game-jvyxu6)
---
+++ Snake.ts
@@ -0,0 +1,34 @@
+import display from './display';
+
+// place your code on line 5 above the export statement below
+
+class Snake {
+  
+  currentPosition: number;
+  currentDirection: number;
+
+  constructor(){
+    this.currentPosition = 0;
+    this.currentDirection = 1;
+  }
+  
+}
+
+move(){
+  if (currentDirection = 1);
+    currentPosition + 1;
+  else 
+    currentPosition - 1;
+}
+
+turn(){
+  if (currentDirection = 1)
+    currentDirection = -1;
+  else
+    currentDirection = 1;
+}
+
+public get position();
+  return this.getPosition;
+
+export default Snake;
