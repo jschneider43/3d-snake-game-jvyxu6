e90cf97730256b2f863b0ea3ce8e38354675a659
jschneider43 <jschneider1@lions.molloy.edu>
2021-02-07 23:50:46 -0500

    wrold model class

--- Snake.ts
+++ Snake.ts
@@ -6,23 +6,71 @@ class Snake {
   
   currentPosition: number;
   currentDirection: number;
+  x: xcoord;
+  y: ycoord;
 
   constructor(){
     this.currentPosition = 0;
     this.currentDirection = 1;
+    this.x = 0;
+    this.y = 0;
   }
 
   move(currentPosition:number){
-    if (currentDirection = 1) {currentPosition + 1;}
-    else {currentPosition - 1;}
+    if (currentDirection = 0) this.position = new Point(this.position.x,this.position.y);
+    else if (currentDirection = 1) this.position = new Point(this.position.x,this.position.y);
+    else if (currentDirection = 2) this.position = new Point(this.position.x,this.position.y);
+    else (currentDirection = 3) this.position = new Point(this.position.x,this.position.y);
   }
 
   turn(){
     if (currentDirection = 1){currentDirection = -1;}
     else {currentDirection = 1;}
   }
+
+  turnLeft(){
+    if(currentDirection = 0) currentDirection + 1;
+    else if(currentDirection = 1) currentDirection + 1;
+    else if(currentDirection = 2) currentDirection + 1;
+    else currentDirection = 3
+  }
+
+  turnRight(){
+ if(currentDirection = 3) currentDirection - 1;
+    else if(currentDirection = 2) currentDirection - 1;
+    else if(currentDirection = 1) currentDirection - 1;
+    else currentDirection = 0
+  }
+
+  set snake(s:snake){
+    this.snake = s;
+  }
 }
   public get position(){
     return this.getPosition;
   }
+
+class Point {
+  private xcoord:number;
+  private ycoord:number;
+
+  constructor(){
+    this.xcoord = xcoord;
+    this.ycoord = ycoord;
+  }
+
+class WorldModel(){
+
+  constructor(){
+    this.snake = s;
+
+  update(steps){
+    
+  }
+  }
+}
+
+}
+  public get x(){}
+  public get y(){}
 export default Snake;
